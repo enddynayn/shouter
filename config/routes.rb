93281d95685babe0202a185cc8b6cf081c189016
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-02 00:33:01 -0700

    create shouts_controller with strong params and add has_many (shouts) association to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Shouter::Application.routes.draw do
   # root page usuall match all HTTP verbs
   # I specify only the a get request with via..
-  root to: 'homes#show', via: :get 
+  root to: 'homes#show', via: :get
+  resources :shouts, only: [:create] 
   # dashboard is a singleton resource because  
   # it does not have an :id
   resource :dashboard, only: [:show] 
