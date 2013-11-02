9ec34a8fbde17af3c129706b1dfa181a387290a7
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-02 00:03:22 -0700

    create model Shout with body and user_id (user: belongs_to)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Shouter::Application.routes.draw do
   # root page usuall match all HTTP verbs
   # I specify only the a get request with via..
   root to: 'homes#show', via: :get 
-
+  # dashboard is a singleton resource because  
+  # it does not have an :id
+  resource :dashboard, only: [:show] 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   
