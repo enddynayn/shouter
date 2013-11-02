20eaf1e202ec770efabc03c4f415db9c887d0f8c
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-02 00:11:06 -0700

    add form to shout in dashboard

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: shouts
+#
+#  id         :integer          not null, primary key
+#  body       :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Shout < ActiveRecord::Base
   belongs_to :user
 end
