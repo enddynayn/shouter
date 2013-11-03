cdaa78f34a2bc6512f008af83a30cdae74755ae6
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-02 18:04:34 -0700

    fixed not rendering problem

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,6 +1,6 @@
 class ShoutsController < ApplicationController
 	def create
-		shout = current_user.shouts.build
+		shout = current_user.shouts.build(shout_parameters)
 		shout.save
 		redirect_to dashboard_path
 	end
