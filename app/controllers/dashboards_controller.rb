9122a5f16e679cffebfb30361027b87ba9ea48b7
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-04 16:30:18 -0800

    generate PhotoShout model image:attachent

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,4 @@
-class DashboardsController < ApplicationController
+ class DashboardsController < ApplicationController
 	def show
 		@text_shout = TextShout.new
 		@shouts = current_user.shouts
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Shouter::Application.routes.draw do
   # root page usuall match all HTTP verbs
   # I specify only the a get request with via..
   root to: 'homes#show', via: :get
-  resources :shouts, only: [:show] 
+ 
   # dashboard is a singleton resource because  
   # it does not have an :id
-  resource :dashboard, only: [:show] 
+  resource  :dashboard, only: [:show] 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :text_shout, only: [:create]
+  resources :shouts, only: [:show]
+  resources :text_shouts, only: [:create]
   
   
 end
