52c65b89759030d411f1209b64a951a217a25d8d
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-04 22:47:11 -0800

    complete textshouts feature

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -14,5 +14,5 @@ class Shout < ActiveRecord::Base
 
   default_scope { order("created_at DESC")}
 
-  validates :body, presence: true
+  belongs_to :content, polymorphic: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Shouter::Application.routes.draw do
   # root page usuall match all HTTP verbs
   # I specify only the a get request with via..
   root to: 'homes#show', via: :get
-  resources :shouts, only: [:show] 
+  
   # dashboard is a singleton resource because  
   # it does not have an :id
   resource :dashboard, only: [:show] 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :shouts, only: [:show] 
   resources :text_shouts, only: [:create]
   
   
