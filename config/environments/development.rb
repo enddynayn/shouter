c4c59e632ac31c119d79eab1e22be533e7462008
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-06 12:22:34 -0800

    fixed the problem with not being able upload pictures by installing imagestick and adding configurations in my conf/env/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Shouter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  ## So paperclip has access to imagick
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
