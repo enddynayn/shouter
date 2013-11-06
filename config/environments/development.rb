1dfb13f9d5b049ad5ec019764ba58da3defb14ef
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-06 13:15:52 -0800

    installed imagestick and configured it in config/enviroment/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Shouter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+# so that paperclip communicates with imagemagik
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
