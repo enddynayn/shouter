061e3d9cff81c6caf39dc98cca2044e5ac7c21a6
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-06 23:37:58 -0800

    following/followed feature works

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: following_relationships
+#
+#  id               :integer          not null, primary key
+#  follower_id      :integer
+#  followed_user_id :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class FollowingRelationship < ActiveRecord::Base
   # when you have a belongs_to it trys to determine the class name 
   # with the association name, such as, class name follower and followed_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,16 @@
 class User < ActiveRecord::Base
 	has_many :shouts, dependent: :destroy
 
-	has_many :following_relationships, foreign_key: :follower_id
-	has_many :followed_users, through: :following_relationships
+	has_many :followed_user_relationships, 
+		foreign_key: :follower_id,
+		class_name: 'FollowingRelationship'
+	has_many :followed_users, through: :followed_user_relationships
+
+	has_many :follower_relationships,
+		foreign_key: :followed_user_id, 
+		class_name: 'FollowingRelationship'
+	has_many :followers, through: :follower_relationships
+
 	# Table: following_relationship
 	# id
 	# follower_id
