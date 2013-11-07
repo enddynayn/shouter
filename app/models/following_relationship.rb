4ce63aa9743d166fd60f56118933263b7e99ed51
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-06 23:05:05 -0800

    followed user relationship works but not followers

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,4 +1,6 @@
 class FollowingRelationship < ActiveRecord::Base
-  belongs_to :follower
-  belongs_to :followed_user
+  # when you have a belongs_to it trys to determine the class name 
+  # with the association name, such as, class name follower and followed_user
+  belongs_to :follower, class_name: 'User' # this overwrites the default behavior
+  belongs_to :followed_user, class_name: 'User'
 end
