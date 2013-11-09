e6bfa45d80128854a1c0ca7af9442042e3003f3d
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-08 18:58:31 -0800

    refractor controller by creating a user varialbe insted of having two user varialbes

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -2,9 +2,7 @@ class FollowingRelationshipsController < ApplicationController
 	def create
 		# params are going to look like 
 		# /users/:user_id/follow(.:format)
-		user = User.find(params[:user_id])
 		current_user.follow user
-		
 		redirect_to user 
 	end
 
@@ -13,7 +11,13 @@ class FollowingRelationshipsController < ApplicationController
 		current_user.unfollow user
 		redirect_to user
 	end
-
+	private
+	# ruby will find a distinction between local
+	# variables and methods. If it cannot find a local variable
+	# it will look for a method. 
 	def user 
-		User.find(params[:user_id])
+		# memoize, I dont want to use this local variables anywhere else. 
+		# underscore to kinda make it private instance variable (no such thing). 
+		@_user ||= User.find(params[:user_id])
+	end
 end
