c2ed36d15e31b50df275529f3154435871a6c25c
Enddy Dumbrique <enddynayn@hotmail.com>
2013-11-08 19:28:04 -0800

    modify the dashboard show to introuduce the dashboard object

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,16 @@
  class DashboardsController < ApplicationController
 	def show
-		@text_shout = TextShout.new
-		@photo_shout = PhotoShout.new
-		@shouts = current_user.shouts
-		puts "%" * 100
-		puts @shouts 
-		puts "%" * 100
-
+		# @text_shout = TextShout.new
+		# @photo_shout = PhotoShout.new
+		# @shouts = current_user.shouts
+		# your controller should only be creating one object
+ #and it should compose the functionality that your view
+ # is going to need
+ 		@dashboard = Dashboard.new(current_user)
 	end
 end
+
+# matz rules (ruby rouges)
+# no class should be more than 100 lines
+# no method should be more than 5 lines
+# can only pass 4 arguments to a method
